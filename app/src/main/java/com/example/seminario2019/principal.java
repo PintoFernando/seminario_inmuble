f149c33dea917b0606fdfb40243cf2512cd425a7
PintoFernando <ghomasricas@gmail.com>
2019-12-04 18:32:30 -0400

    primero

---
+++ app/src/main/java/com/example/seminario2019/principal.java
@@ -0,0 +1,11 @@
+package com.example.seminario2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class principal   extends AppCompatActivity {
+
+
+    }
---
+++ app/src/main/java/com/example/seminario2019/register.java
@@ -0,0 +1,32 @@
+package com.example.seminario2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class register extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+       /* log = (Button) findViewById(R.id.btnlogin);
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent log = new Intent(MainActivity.this, register.class);
+                startActivity(log);
+
+
+            }
+
+        });
+    }*/
+    }
+}
