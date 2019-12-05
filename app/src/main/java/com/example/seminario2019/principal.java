23e37e7ea2134ec2c29e7bb035bee86da6633c6e
PintoFernando <ghomasricas@gmail.com>
2019-12-05 12:42:39 -0400

    proyecto1

--- app/src/main/java/com/example/seminario2019/principal.java
+++ app/src/main/java/com/example/seminario2019/principal.java
@@ -4,8 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class principal   extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+
+
+
+
+
+
+
+    }
 
 
     }
